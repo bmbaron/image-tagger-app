b9c6c825527e228b91bad112f100165f1fd8268f
Ben Baron <benjamin.m.baron@gmail.com>
2022-09-10 10:54:27 +0700

    added end of game checking

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
   const [select, setSelect] = useState("")
 
   const [foundAnimals, setFoundAnimals] = useState({"rabbit": false, "snake": false, "spider": false})
+  const [allFound, setAllFound] = useState(false)
   const [wrongAnswer, setWrongAnswer] = useState(false)
   const [correctAnswer, setCorrectAnswer] = useState(false)
 
@@ -42,6 +43,12 @@ function App() {
       setCorrectAnswer(false)
     }, 3000)
   }, [wrongAnswer, correctAnswer])
+
+  useEffect(() => {
+    if (!Object.values(foundAnimals).includes(false)) {
+      setAllFound(true)
+    }
+  }, [foundAnimals])
 
   function initializeImage(res) {
     setImage(res[0].url)
@@ -68,8 +75,8 @@ function App() {
       let boxStartY = Number((((event.clientY-event.currentTarget.getBoundingClientRect().top)-25)/myRef.current.clientHeight).toFixed(2))
       let boxEndY = Number((((event.clientY-event.currentTarget.getBoundingClientRect().top)+50)/myRef.current.clientHeight).toFixed(2))
 
-      console.log((event.clientX-event.currentTarget.getBoundingClientRect().left)/myRef.current.clientWidth)
-      console.log((event.clientY-event.currentTarget.getBoundingClientRect().top)/myRef.current.clientHeight)
+      // console.log((event.clientX-event.currentTarget.getBoundingClientRect().left)/myRef.current.clientWidth)
+      // console.log((event.clientY-event.currentTarget.getBoundingClientRect().top)/myRef.current.clientHeight)
 
       setClickLocation([boxStartX, boxStartY])
       setMarginLeft(event.currentTarget.getBoundingClientRect().left)
@@ -91,8 +98,13 @@ function App() {
         setFoundAnimals(prev => ({...prev, [event.target.value]: true}))
         setCorrectAnswer(true)
         setTimeout(()=>{
-          getNext()
-        }, 2000)
+          if (Object.values(foundAnimals).filter(x => x === true).length < 2) {
+            getNext()
+          }
+        }, 1000)
+      }
+      else {
+        setWrongAnswer(true)
       }
     }
     else {
@@ -103,6 +115,7 @@ function App() {
 
   function getNext() {
     if(null !== imageData) {
+      setClickLocation([0,0])
       setIsLoading(true)
       setImage(imageData[imageCounter].url)
       setAnimal(imageData[imageCounter].animal)
@@ -134,7 +147,10 @@ function App() {
         style={{display: isLoading ? "none" : "block"}}
       />
       <div className="info">
-        <div className={correctAnswer ? "status-box correct": "status-box"}>
+        <div
+          className={correctAnswer ? "status-box correct": "status-box"}
+          style={{ backgroundColor: allFound && 'rgba(31, 222, 53, 0.5)' }}
+        >
           <h1 className="status-title">Hidden Animals</h1>
           <div className="animal-targets">
           <div className={foundAnimals["rabbit"] ? "found" : ""}>
@@ -152,14 +168,15 @@ function App() {
           </div>
           <button className="next-pic-button" onClick={getNext}>next picture</button>
         </div>
-        <h1 className="instructions">find one animal per picture and tag their heads</h1>
+        <h1 className="instructions">{allFound ? "You found them all!": "Each picture has 1 animal. Tag their heads."}</h1>
       </div>
 
       {null !== myRef.current && <div className={wrongAnswer ? "target-box wrong" : "target-box"} 
+        //don't show when loading image, position in middle of screen when image loads, position at user's click after 
         style={{
           display: isLoading ? 'none' : 'block',
-          left: clickLocation[0]*imgWidth+marginLeft,
-          top: clickLocation[1]*imgHeight,
+          left: clickLocation[0] === 0 ? (window.innerWidth / 2) - 25 : clickLocation[0]*imgWidth+marginLeft,
+          top: clickLocation[0] === 0 ? (window.innerHeight / 2) - 25 : clickLocation[1]*imgHeight,
         }}
       >
         <select
